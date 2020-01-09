6246905bb47e199f4bc479f49dcb9ebf9b4c6bc2
unknown <dzhytnik@gmail.com>
2020-01-09 16:57:25 +0200

    Added db script

---
+++ database_scripts/script.sql
@@ -0,0 +1,28 @@
+CREATE TABLE public.gsm_provider
+(
+    id integer NOT NULL DEFAULT nextval('gsm_provider_id_seq'::regclass),
+    provider_name character varying(100) COLLATE pg_catalog."default",
+    primary_url character varying(500) COLLATE pg_catalog."default",
+    secondary_url character varying(500) COLLATE pg_catalog."default",
+    username character varying(100) COLLATE pg_catalog."default",
+    user_password character varying(100) COLLATE pg_catalog."default",
+    CONSTRAINT gsm_provider_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.gsm_provider
+    OWNER to postgres;
+
+CREATE TABLE public.global_config
+(
+    id integer NOT NULL DEFAULT nextval('global_config_id_seq'::regclass),
+    number_of_attempts numeric,
+    secondary_channel_timeslot numeric,
+    CONSTRAINT global_config_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.global_config
+    OWNER to postgres;
